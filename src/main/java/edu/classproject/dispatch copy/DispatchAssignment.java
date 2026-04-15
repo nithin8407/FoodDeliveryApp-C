a7748f5d686e798cf3bc09c57f556117e321d632
nithin8407 <your_github_email@example.com>
2026-04-15 12:55:39 +0530

    Dispatch Module Submission (Team 12)

--- src/main/java/edu/classproject/dispatch copy/DispatchAssignment.java
+++
@@ -1,4 +0,0 @@
-package edu.classproject.dispatch;
-
-public record DispatchAssignment(String assignmentId, String orderId, String partnerId, String reason) {
-}
--- src/main/java/edu/classproject/dispatch copy/DispatchService.java
+++
@@ -1,5 +0,0 @@
-package edu.classproject.dispatch;
-
-public interface DispatchService {
-    DispatchAssignment assignPartner(String orderId, String restaurantId, String customerId);
-}
